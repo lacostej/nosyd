e52370e2d5c48ee96251d01b0ff55149b89de7ab
jerome.lacoste <jerome.lacoste@gmail.com>
2009-09-24 23:50:30 +0200

    inline the imports until I find out how to properly package a python app

--- nosy/nosy.py
+++ nosy/nosy.py
@@ -5,7 +5,70 @@
 import glob,os,stat,time,os.path
 import pynotify
 import logging
-from xunit import *
+
+############################################################################
+# inline the imports until I find out how to properly package a python app #
+############################################################################
+def findall(seq, f):
+    """Return all the element in seq where f(item) == True."""
+    result = []
+    for element in seq:
+      if f(element):
+        result.append(element)
+    return result
+
+#from utils import *
+
+class XunitTestSuite:
+  def __init__(self, name, tests, errors, failures, skip, testcases):
+    self.name = name
+    self.tests = tests
+    self.errors = errors
+    self.failures = failures
+    self.skip = skip
+    self.testcases = testcases
+
+  def __str__(self):
+    return "XunitTestSuite: %s %s %s %s %s" % (self.name , self.tests, self.errors, self.failures, self.skip)
+
+  def list_failure_names(self):
+    failed_testcases = findall(self.testcases, lambda tc : tc.failed())
+    return [ el.name for el in failed_testcases ]
+
+class TestCase:
+  def __init__(self, classname, name, failure):
+    self.classname = classname
+    self.name = name
+    self.failure = failure
+
+  def failed(self):
+    return self.failure != None
+
+class Failure:
+  def __init__(self, type, text):
+    self.type = type
+    self.text = text
+
+def parse_xunit_results(filename):
+  try :
+    from xml.dom import minidom
+    xmldoc = minidom.parse(filename)
+    testsuite = xmldoc.firstChild
+    tcs = testsuite.getElementsByTagName('testcase')
+    testcases = []
+    for tc in tcs:
+      failure = None
+      if (len(tc.childNodes) > 0):
+        failureNode = tc.childNodes[0]
+        failure = Failure(failureNode.attributes['type'].value, failureNode.childNodes[0].data)
+      testcases.append(TestCase(tc.attributes['classname'].value, tc.attributes['name'].value, failure))
+    return XunitTestSuite(testsuite.attributes['name'].value, int(testsuite.attributes['tests'].value), int(testsuite.attributes['errors'].value), int(testsuite.attributes['failures'].value), int(testsuite.attributes['skip'].value), testcases)  
+  except IOError:
+    return None
+
+#from xunit import *
+############################################################################
+
 
 logger = logging.getLogger("simple_example")
 LEVELS = {'debug': logging.DEBUG,
