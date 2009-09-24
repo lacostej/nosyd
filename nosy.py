33b18e62c22db1682c9f1bc9172a1c33dd8cb748
jerome.lacoste <jerome.lacoste@gmail.com>
2009-09-24 04:22:26 +0200

    quick and dirty add other directories to monitor. We should really configure this

--- nosy.py
+++ nosy.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # By Jeff Winkler, http://jeffwinkler.net
 
 import glob,os,stat,time
@@ -9,7 +10,13 @@ def checkSum():
     ''' Return a long which can be used to know if any .py files have changed.
     Only looks in the current directory. '''
     val = 0
+    for f in glob.glob ('*.txt') + glob.glob ('data/*.txt'):
+        stats = os.stat (f)
+        val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
     for f in glob.glob ('*.py'):
+        stats = os.stat (f)
+        val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
+    for f in glob.glob ('tests/*.py'):
         stats = os.stat (f)
         val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
     for f in glob.glob ('*.kid'):
