c43ae66a25373549b7d4ff42b3972b8bfb3d7c91
jerome.lacoste <jerome.lacoste@gmail.com>
2009-10-05 16:16:49 +0200

    Add setuptools config/install file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+djl_url = "http://github.com/lacostej/nosyd"
+version = "0.0.3"
+
+setup(
+    name="Nosyd",
+    version=version,
+    description="""\
+A _minimalist_ personal command line friendly CI server. Automatically runs your build whenever one
+of the monitored files of the monitored projects has changed.
+    """,
+    long_description="""\
+A daemonization of Jeff Wrinkler's original nosy script that automatically
+runs your build whenever one of the monitored files of the monitored projects has changed. This version
+has a command line interface, supports multiple builders, uses configuration files and desktop notifications.
+""",
+    author="Jeff Winkler & Jerome Lacoste",
+    author_email="jerome.lacoste@gmail.com",
+    url=djl_url,
+    download_url="%(djl_url)s/tarball/%(version)s" % locals(),
+    packages=find_packages(),
+    install_requires=['nose'],
+    entry_points={'console_scripts':['nosyd = nosyd.nosyd:main']}
+    )
+
+# end of file
